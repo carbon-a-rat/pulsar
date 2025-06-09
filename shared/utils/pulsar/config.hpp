5cb7fb2899a1916b5baf281af47d6d17a95e47d2
cyp <supercyp@protonmail.com>
2025-06-09 07:44:27 +0100

    misc: update config
    Me ? accidently leaking my password ? after more than 6 years of programming ? I would never do this kind of thing !

--- shared/utils/pulsar/config.hpp
+++ shared/utils/pulsar/config.hpp
@@ -1,5 +1,8 @@
 #pragma once 
 
 
+// this is my old password as an example, (No I never accidentally leaked it, I would never do this ! Me ? NAH ! ) 
+
+
 #define USER_NAME "supercyp971@gmail.com"
 #define USER_PASSWORD "Petted-Mosaic-Stargazer-Skinless2-Overhung-Huddle"
